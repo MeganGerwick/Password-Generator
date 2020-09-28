db6995be9487949262d57244564aa3df3a14d5bd
Louisa May <mgerwick@gmail.com>
2020-09-28 18:25:20 -0500

    added prompts for special characters, upper and lower case letters, and numeric

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//When button is clicked, user is prompted for password criteria. Prompt asks length of password needed
+//When button is clicked, user is prompted for password criteria. 
 document.getElementById("generate").onclick = function () {
     prompt("How many characters does your password need to be? (Response must be between 8 and 128)")
+    confirm("Do you need lowercase and uppercase letters?");
+    confirm("Do you need a numeric character?");
+    confirm("Do you need special characters? (!, #, $, etc.)");
 };
 
 
 //Prompt asks for character types needed (lowercase, uppercase, numeric, and/or special characters)
+
 
 //Generate a password 
 
