237f1f46cba1d8f1001d94481653c897c057dfe0
Louisa May <mgerwick@gmail.com>
2020-09-28 18:40:17 -0500

    added arrays for all character options

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function writePassword() {
     passwordText.value = password;
 
 }
+// Set arrays for characters, lower case, upper case, special, and numeric
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["!", "$", "#", "%", "&", "+", "*", "-"];
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -23,6 +28,7 @@ document.getElementById("generate").onclick = function () {
 
 
 //Prompt asks for character types needed (lowercase, uppercase, numeric, and/or special characters)
+
 
 
 //Generate a password 
